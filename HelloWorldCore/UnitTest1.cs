52f86b9e76273a26beb29d4789d62bbe0c69c60a
Nitin Kumar <nitin@nitins-MacBook-Air.local>
2020-10-03 09:31:08 +0530

    UnitTest
    Assignment 2

---
+++ HelloWorldCore/UnitTest1.cs
@@ -0,0 +1,23 @@
+using System;
+using Xunit;
+
+namespace HelloWorldCore
+{
+    public class UnitTest1
+    {
+
+
+        public static void func()
+        {
+            Console.WriteLine("Hello World!");
+
+        }
+        [Fact]
+        public void Test1()
+        {
+
+        }
+
+
+    }
+}
